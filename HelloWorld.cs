6d756d0e870905117aa26af79f03a70709007caf
Samantha Mayes <SamanthaTmayes@gmail.com>
2026-01-16 11:08:44 -0700

    main

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MusicPlayerManager
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
